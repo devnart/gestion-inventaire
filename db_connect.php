2ed0c119215120a33d5c490171ce23b19dba64ca
devnart <hr.fb001@gmail.com>
2021-03-16 22:05:06 +0100

    test remote db

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,10 @@
 <?php 
-    //connect to database
-$connection = mysqli_connect('localhost', 'root', '', 'stock');
+    //connect to database LOCAL
+// $connection = mysqli_connect('localhost', 'root', '', 'stock');
+
+// Remote Connect
+$connection = mysqli_connect('remotemysql.com', 'O4hkw6m8zM', 'JoXTxPjP0f', 'O4hkw6m8zM');
+
 
 //check connection
 if (!$connection) {
